e1afe3c109e44aa9e0de3904cc15d078072e2d01
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-11-11 18:33:24 +0100

    .

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,17 +1,42 @@
-const AddNote = () => {
+import {useState} from "react";
+import note from "./Note";
+
+const AddNote = ({handleAddNote}) => {
+    const [noteText, setNoteText] = useState('');
+
+    //character limit
+    const characterLimit = 200;
+
+    const handleChange = (event) => {
+        //nepojde pod 0
+        if(characterLimit - event.target.value.length >= 0) {
+        setNoteText(event.target.value);
+        }
+    };
+
+    const handleSaveClick = () => {
+        if(noteText.trim().length > 0) {
+            handleAddNote(noteText)
+            setNoteText('');
+        }
+    };
+
     return (
         <div className='note new'>
             <textarea
                 rows="8"
                 cols="10"
                 placeholder="Type to add a note..."
+                value = {noteText}
+                onChange={handleChange}
             ></textarea>
             <div className="note-footer">
-                <small>200 Remaining</small>
-                <button className="save">Save</button>
+                <small>{characterLimit - noteText.length} Remaining</small>
+                <button className="save" onClick={handleSaveClick}>Save</button>
             </div>
         </div>
     );
 };
-//33:09 pokracovanie
+
+
 export default AddNote;
